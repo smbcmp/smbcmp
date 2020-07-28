779ed5a95ffb29292ea4fa3b6dca4124b672feef
Aurelien Aptel <aaptel@suse.com>
2020-07-28 12:37:09 +0200

    export split_packet_arg() in smbcmp module

--- smbcmp/__init__.py
+++ smbcmp/__init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 # explicit export for cleaner namespace
-from smbcmp.common import KEY, CONF, parse_args, smb_packet, smb_summaries, smb_diff, PDMLDiff, strip_packet_no
+from smbcmp.common import KEY, CONF, parse_args, smb_packet, smb_summaries, smb_diff, PDMLDiff, strip_packet_no, split_packet_arg
